8fdf2167bc5a1341c9e52f3f0fbeffc0923618fd
wh11e7rue <wh11e7rue@icloud.com>
2015-09-15 12:19:00 +0900

    Work on Python 3

--- src/main/resources/mcpi/api/python/modded/mcpi/util.py
+++ src/main/resources/mcpi/api/python/modded/mcpi/util.py
@@ -1,10 +1,28 @@
 import collections
 
+
+try:
+    unicode = unicode
+except NameError:
+    # 'unicode' is undefined, must be Python 3
+    str = str
+    unicode = str
+    bytes = bytes
+    basestring = (str,bytes)
+else:
+    # 'unicode' exists, must be Python 2
+    str = str
+    unicode = unicode
+    bytes = str
+    basestring = basestring
+
+
 def flatten(l):
     for e in l:
         if isinstance(e, collections.Iterable) and not isinstance(e, basestring):
             for ee in flatten(e): yield ee
         else: yield e
+
 
 def flatten_parameters_to_string(l):
     return ",".join(map(str, flatten(l)))
